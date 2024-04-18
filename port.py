19cc335f7e67fdd1cc437ce325d149e8a06cae84
shahi.subash79@gmail.com <shahi.subash79@gmail.com>
2024-04-18 22:05:33 +0930

    Updates for start time and end time ,  form updated

--- port.py
+++ port.py
@@ -1,31 +1,50 @@
+import time
+import socket
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
-import socket
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 
 class PortScanResult:
-    def __init__(self, port, status, service=None, protocol=None, service_product=None):
+    def __init__(self, port, status, start_time=None, finish_time=None, service=None, protocol=None, service_product=None):
         self.port = port
         self.status = status
+        self.start_time = start_time
+        self.finish_time = finish_time
         self.service = service
         self.protocol = protocol
         self.service_product = service_product
 
 
+def get_service_name(port):
+    try:
+        service = socket.getservbyport(port)
+        return service
+    except OSError:
+        return "Unknown"
+
+
 def scan_ports(target, start_port, end_port, port_type):
+    open_count = 0
+    closed_count = 0
+    filtered_count = 0
     protocol = None
     service_product = None
     results = []
     service = ""
+    start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     for port in range(start_port, end_port + 1):
         service = ""
+        # start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)  # Timeout for connection attempt
         result = sock.connect_ex((target, port))
         sock.close()
+        # finish_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         if port_type == 1 and result == 0:
+            open_count += 1
             try:
                 service = socket.getservbyport(port)
             except OSError:
@@ -35,11 +54,16 @@ def scan_ports(target, start_port, end_port, port_type):
                 port, "open", service, protocol, service_product))
         # Connection refused (port closed)
         elif port_type == 2 and result == 11:
-            results.append(PortScanResult(port, "closed"))
+            closed_count += 1
+            results.append(PortScanResult(
+                port, "closed"))
         elif port_type == 3 and result != 0 and result != 11:
-            results.append(PortScanResult(port, "filtered"))
+            filtered_count += 1
+            results.append(PortScanResult(
+                port, "filtered"))
         elif port_type == 0:
             if result == 0:
+                open_count += 1
                 try:
                     service = socket.getservbyport(port)
                 except OSError:
@@ -48,11 +72,17 @@ def scan_ports(target, start_port, end_port, port_type):
                 results.append(PortScanResult(
                     port, "open", service, protocol, service_product))
             elif result == 11:
-                results.append(PortScanResult(port, "closed"))
+                closed_count += 1
+                results.append(PortScanResult(
+                    port, "closed"))
             else:
-                results.append(PortScanResult(port, "filtered"))
+                filtered_count += 1
+                results.append(PortScanResult(
+                    port, "filtered"))
 
-    return results
+    finish_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    return results, open_count, closed_count, filtered_count, start_time, finish_time
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -63,11 +93,11 @@ def index():
         end_port = int(request.form['end_port'])
         port_type = int(request.form['port_type'])
 
-        scan_results = scan_ports(
+        scan_results, open_count, closed_count, filtered_count, start_time, finish_time = scan_ports(
             target, start_port, end_port, port_type)
 
         return render_template('index.html', target=target, start_port=start_port, end_port=end_port,
-                               scan_results=scan_results)
+                               scan_results=scan_results, open_count=open_count, closed_count=closed_count, filtered_count=filtered_count, start_time=start_time, finish_time=finish_time)
     return render_template('index.html')
 
 
